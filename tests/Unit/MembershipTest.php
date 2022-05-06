f499c504c46805fc6fe43304d25f064be9ba90c3
Taburetkastol <niklimonoff@yandex.ru>
2022-05-06 10:26:41 +0300

    Update MembershipTest.php

--- tests/Unit/MembershipTest.php
+++ tests/Unit/MembershipTest.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Models\Membership;
+use App\Models\Musician;
+use App\Models\Band;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
+uses(RefreshDatabase::class);
 uses(Tests\TestCase::class);
 
 test('create membership', function () {
+    Musician::factory()->create();
+    Band::factory()->create();
     $attributes = Membership::factory()->raw();
     $response = $this->postJson('/api/v1/membership', $attributes);
     $response->assertStatus(200);
@@ -12,6 +18,8 @@ test('create membership', function () {
 });
 
 test('get membership', function () {
+    Musician::factory()->create();
+    Band::factory()->create();
     $membership = Membership::factory()->create();
 
     $response = $this->getJson("/api/v1/membership/{$membership->id}");
@@ -34,6 +42,8 @@ test('get membership', function () {
 });
 
 test('put membership', function () {
+    Musician::factory()->create();
+    Band::factory()->create();
     $membership = Membership::factory()->create();
     $updatedMembership = ['musician_id' => Membership::inRandomOrder()->first()->musician_id];
     $response = $this->putJson("/api/v1/membership/{$membership->id}", $updatedMembership);
@@ -42,6 +52,8 @@ test('put membership', function () {
 });
 
 test('patch membership', function () {
+    Musician::factory()->create();
+    Band::factory()->create();
     $membership = Membership::factory()->create();
     $updatedMembership = ['musician_id' => Membership::inRandomOrder()->first()->musician_id];
     $response = $this->patchJson("/api/v1/membership/{$membership->id}", $updatedMembership);
@@ -50,12 +62,16 @@ test('patch membership', function () {
 });
 
 test('delete membership', function () {
+    Musician::factory()->create();
+    Band::factory()->create();
     $membership = Membership::factory()->create();
     $response = $this->deleteJson("/api/v1/membership/{$membership->id}");
     $response->assertStatus(200);
 });
 
 test('get failed membership', function () {
+    Musician::factory()->create();
+    Band::factory()->create();
     $membership = Membership::factory()->create();
 
     $response = $this->getJson("/api/v1/membership/asdf");
